8c3a03ed58f69fccf50cf82acef881d6858a81f5
Akash Palukuri <akashp@vt.edu>
2025-02-01 19:21:54 -0500

    Update app.js
    changed bracket placement

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -117,7 +117,7 @@ while (currentIndex != 0) {
     array[randomIndex],
     array[currentIndex],
   ];
-}
+
 /* This is for the gifs method */
 // icons[0].innerHTML = `<img src ="${gifArray[0]}" alt="Skeleton">`;
 //icons[1].innerHTML = `<img src ="${gifArray[1]}" alt="Key">`;
@@ -130,5 +130,5 @@ icons[1].innerText = array[1];
 icons[2].innerText = array[2];
 let emojiSting = array[0] + array[1] + array[2];
 console.log(emojiSting);
-
+}
 Window.wordList = wordList;
